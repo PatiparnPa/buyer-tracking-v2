c4bb26118ce9c3890685edf7c4eeac3b45902ee1
zar5789 <112923866+zar5789@users.noreply.github.com>
2024-03-11 15:53:18 +0700

    update

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import Cart2 from "../assets/cart2.png"
+import Cart2 from "../assets/cart2.png";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -6,16 +6,36 @@ const MyAppBar = () => {
   const navigate = useNavigate();
 
   const handleCartClick = () => {
-    navigate('/mycart');
+    navigate("/mycart");
   };
   return (
-    <div className="app-bar" style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
-      <h5 style={{ marginTop: '3%', marginLeft:'3%', fontWeight:'500' }}><Link style={{textDecoration:'none', color:'inherit'}} to='/load'>IT CAFETERIA</Link></h5>
+    <div
+      className="app-bar"
+      style={{ position: "fixed", top: 0, width: "100%", zIndex: 1000 }}
+    >
+      <h5 style={{ marginTop: "3%", marginLeft: "3%", fontWeight: "500" }}>
+        <Link style={{ textDecoration: "none", color: "inherit" }} to="/load">
+          IT CAFETERIA
+        </Link>
+      </h5>
       <div className="right-elements">
         <div className="elements-container">
-              <button onClick={handleCartClick} style={{ background: 'none', border: 'none', cursor: 'pointer', marginLeft: '5px', marginTop:'5px' }}>
-                <img src={Cart2} alt="Cart" style={{ width: '33px', height: '33px'}} />
-              </button>
+          <button
+            onClick={handleCartClick}
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              marginLeft: "5px",
+              marginTop: "5px",
+            }}
+          >
+            <img
+              src={Cart2}
+              alt="Cart"
+              style={{ width: "33px", height: "33px" }}
+            />
+          </button>
         </div>
       </div>
     </div>
