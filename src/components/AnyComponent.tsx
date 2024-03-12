91012454881036e9e8dcb6e015d0d3aaa70100cc
zar5789 <112923866+zar5789@users.noreply.github.com>
2024-03-12 13:04:37 +0700

    token

--- src/components/AnyComponent.tsx
+++ src/components/AnyComponent.tsx
@@ -4,6 +4,7 @@ import PopupComponent from "./PopupComponent";
 import { useState } from "react";
 import React, { useEffect } from "react";
 import { useUser } from "./UserContext";
+import { jwtDecode } from "jwt-decode";
 
 const AnyComponent: React.FC = () => {
   const { isPopupOpen, openPopup, closePopup } = usePopup();
@@ -14,6 +15,23 @@ const AnyComponent: React.FC = () => {
   console.log("User ID:", userData.userId);
   console.log("Basket ID:", userData.basketId);
   console.log("Favorite ID:", userData.favoriteId);
+
+  useEffect(() => {
+    // Retrieve access token from local storage (or wherever it's stored)
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      try {
+        // Decode the access token
+        const decodedToken: { [key: string]: any } = jwtDecode(accessToken);
+        // Extract user-related data from the decoded token
+
+        console.log("UserData", decodedToken);
+      } catch (error) {
+        console.error("Error decoding access token:", error);
+      }
+    }
+  }, []);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
